614451180dbe119ad06a26f439257f6ec602b672
dmaphy <dmaphy@2d73f865-6be7-0310-b98f-a90c8a95c1fd>
2005-01-20 17:03:07 +0000

    testklasse

---
+++ sources/DReadData.java
@@ -0,0 +1,65 @@
+/****************************************************************************
+ *   Copyright (C) 2005 by BTU SWP GROUP 04/6.1                             *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by   *
+ *   the Free Software Foundation; either version 2 of the License, or	    *
+ *   (at your option) any later version                                     *
+ *                                                                          *
+ *   This program is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU General Public License for more details                            *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program; if not, write to the                          *
+ *   Free Software Foundation, Inc.,                                        *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.              *
+ ***************************************************************************/
+
+/**
+	Debug
+	Testklasse zum auslesen der Daten,
+	kann evtl. in zeichneKurve übernommen und bearbeitet werden.
+*/
+
+import java.util.ArrayList;
+
+class DReadData {
+	private ArrayList werte;
+	private ArrayList daten;
+
+	public DReadData(){
+		this.werte = wedabecha.getKurve(1).getWerte();
+		this.daten = wedabecha.getKurve(1).getDaten();
+		this.printWerte();
+		this.printDaten();
+	} // DReadData()
+
+	private void printWerte(){
+		int statArray[];
+		String debug;
+		for (int i = 0; i < this.werte.size(); i++){
+			statArray = (int[])this.werte.get(i);
+			debug = "";
+			for (int j = 0; j < statArray.length; j++){
+				debug += statArray[j];
+			} // for(j)
+			System.out.println(debug);
+		} // for(i)
+
+	} // printWerte()
+
+	private void printDaten(){
+		int statArray[];
+		String debug;
+		for (int i = 0; i < this.daten.size(); i++){
+			statArray = (int[])this.daten.get(i);
+			debug = "";
+			for (int j = 0; j < statArray.length; j++){
+				debug += statArray[j];
+			} // for(j)
+			System.out.println(debug);
+		} // for(i)
+	} // printDaten()
+} // DReadData
